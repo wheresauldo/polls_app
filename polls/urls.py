9397b6fd5921e51a2c6c0b499d56e7c8b045215e
Auldo <dionaulden@gmail.com>
2019-11-06 16:46:35 -0500

    Working version

---
+++ polls/urls.py
@@ -0,0 +1,21 @@
+from django.urls import path
+
+from . import views
+
+
+
+
+app_name = 'polls'
+
+urlpatterns = [
+    #index
+    path('', views.IndexView.as_view(), name='index'),
+    #detail
+    path('<int:pk>/',views.DetailView.as_view(), name='detail'),
+    # results
+    path('<int:pk>/results/', views.ResultsView.as_view(), name='results'),
+    #vote 
+    path('<int:question_id>/vote/', views.vote, name='vote'),
+
+
+]
